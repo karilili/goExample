4883c012df8f7ff1de11b41a7b652cb54adebcf8
karilili <386863093@qq.com>
2023-04-10 15:07:40 +0800

    第四节课 用户业务封装

--- GolangStudy/14-golang-IM-System/user.go
+++ GolangStudy/14-golang-IM-System/user.go
@@ -7,10 +7,12 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
+
+	server *Server
 }
 
 // 创建一个用户的API
-func NewUser(Conn net.Conn) *User {
+func NewUser(Conn net.Conn, server *Server) *User {
 	userAddr := Conn.RemoteAddr().String()
 
 	user := &User{
@@ -18,11 +20,41 @@ func NewUser(Conn net.Conn) *User {
 		Addr: userAddr,
 		C:    make(chan string),
 		conn: Conn,
+
+		server: server,
 	}
 
 	//启动监听当前user channel消息的goroutine
 	go user.ListenMessage()
 	return user
+}
+
+// 用户的上线业务
+func (this *User) Online() {
+
+	//用户上线，将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	//广播当前用户上线消息
+	this.server.BroadCast(this, "已上线")
+}
+
+// 用户的下线功能
+func (this *User) Offline() {
+	// 用户上线，将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户下线消息
+	this.server.BroadCast(this, "已下线")
+}
+
+// 用户处理消息的业务
+func (this *User) DoMessage(msg string) {
+    this.server.BroadCast(this,msg)
 }
 
 // 监听当前User channel的方法，一旦有消息，就直接发送给对端客户端
