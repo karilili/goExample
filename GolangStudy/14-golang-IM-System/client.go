6b027584b54ca74815bc1dd9cbde3637829de2c0
karilili <386863093@qq.com>
2023-04-11 14:57:47 +0800

    第十四节课 客户端功能-私聊模式

--- GolangStudy/14-golang-IM-System/client.go
+++ GolangStudy/14-golang-IM-System/client.go
@@ -67,6 +67,48 @@ func (client *Client) menu() bool {
 	}
 }
 
+// 查询在线用户
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn Write err:", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	client.SelectUsers()
+	fmt.Println(">>>>>>请输入聊天对象[用户名],exit退出:")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println(">>>>>>请输入聊天内容,exit退出..")
+		fmt.Scanln(&chatMsg)
+
+		for chatMsg != "exit" {
+			//消息不为空则发送
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn Write err:", err)
+					break
+				}
+			}
+			chatMsg = ""
+			fmt.Println(">>>>>请输入聊天内容,exit退出..")
+			fmt.Scanln(&chatMsg)
+		}
+		client.SelectUsers()
+		fmt.Println(">>>>>>请输入聊天对象[用户名],exit退出:")
+		fmt.Scanln(&remoteName)
+	}
+}
+
 func (client *Client) PubliChat() {
 	//提示用户输入信息
 	var chatMsg string
@@ -119,6 +161,7 @@ func (client *Client) Run() {
 		case 2:
 			//私聊模式
 			fmt.Println("私聊模式选择...")
+			client.PrivateChat()
 			break
 		case 3:
 			//修改用户名
